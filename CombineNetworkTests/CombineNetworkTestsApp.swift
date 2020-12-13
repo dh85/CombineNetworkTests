9c5072a2ba6a0577b00e573bd6774bbb933d1c25
David Hughes <david.6.hughes@gmail.com>
2020-12-13 16:34:40 +0000

    Initial Commit

---
+++ CombineNetworkTests/CombineNetworkTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineNetworkTestsApp.swift
+//  CombineNetworkTests
+//
+//  Created by David Hughes on 13/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CombineNetworkTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
